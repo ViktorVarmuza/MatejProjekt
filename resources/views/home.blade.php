31c9de09d7b557c53ce6936799f713c9f5674832
Oslik9 <vosahlomatej9@seznam.cz>
2026-04-29 09:19:23 +0200

    pager konec hodiny 29.4.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,55 +1,36 @@
 @extends('template.main')
 
-
 @section('content')
 
 <div class="p-10">
 
     <h1 class="text-3xl font-bold mb-8 text-center">Závody</h1>
 
-
-
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
         @foreach($data as $race)
+            <div class="bg-white shadow rounded-lg p-5">
 
-        <div class="bg-white shadow rounded-lg p-5">
+                <h2 class="text-xl font-semibold mb-4">
+                    {{ $race->default_name }}
+                </h2>
 
-            <h2 class="text-xl font-semibold mb-4">
+                <a href="{{ route('race', $race->id) }}">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded">
+                        Detail
+                    </button>
+                </a>
 
-                {{ $race->default_name }}
-
-            </h2>
-
-
-
-            <a href="{{ route('race', $race->id) }}">
-
-                <button class="bg-blue-500 text-white px-4 py-2 rounded">
-
-                    Detail
-
-                </button>
-
-            </a>
-
-
-
-        </div>
-
+            </div>
         @endforeach
 
-
-
     </div>
 
+    {{-- Pagination center --}}
+<div class="mt-12 flex justify-center">
+    {{ $data->onEachSide(1)->links() }}
+</div>
 
+</div>
 
-    <!-- stránkování -->
-
-    <div class="mt-8 flex justify-center">
-
-        {{ $data->links() }}
-
-    </div>
-    @endsection
+@endsection
