b112d841d2d81c054d2254868ed0737eb9ba1072
ViktorVarmuza <varmuza_viktor@oauh.cz>
2026-04-29 09:23:23 +0200

    konec hodiny

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,6 @@
 <div class="p-10">
 
     <h1 class="text-3xl font-bold mb-8 text-center">Závody</h1>
-
-
-
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
         @foreach($data as $race)
@@ -20,9 +17,6 @@
                 {{ $race->default_name }}
 
             </h2>
-
-
-
             <a href="{{ route('race', $race->id) }}">
 
                 <button class="bg-blue-500 text-white px-4 py-2 rounded">
@@ -32,20 +26,9 @@
                 </button>
 
             </a>
-
-
-
         </div>
-
         @endforeach
-
-
-
     </div>
-
-
-
-    <!-- stránkování -->
 
     <div class="mt-8 flex justify-center">
 
